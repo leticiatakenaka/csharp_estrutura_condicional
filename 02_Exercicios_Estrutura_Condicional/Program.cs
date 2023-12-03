116414f47e6268d51f42f8d75282769e92c467a4
leticia.takenaka <leticia.takenaka@BHN050103221>
2023-12-03 11:23:39 -0300

    Exercício 1: Número negativo ou positivo

--- 02_Exercicios_Estrutura_Condicional/Program.cs
+++ 02_Exercicios_Estrutura_Condicional/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿//Fazer um programa para ler um número inteiro, e depois dizer se este número é negativo ou não.
+
+Console.Write("Entre com um número inteiro: ");
+int numero = int.Parse(Console.ReadLine());
+
+Console.WriteLine(MensagemNumeroInteiro(numero));
+
+string MensagemNumeroInteiro(int n)
+{
+    if (n == 0)
+        return "O número é 0";
+
+    if (n > 0)
+        return "O número é positivo";
+
+    return "O número é negativo";
+}
