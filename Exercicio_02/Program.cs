1f9d3b6244fadfaa4bfc758cebd767a1ae2fe42e
leticia.takenaka <leticia.takenaka@BHN050103221>
2023-12-03 11:31:46 -0300

    Exercício 2: Par ou ímpar

---
+++ Exercicio_02/Program.cs
@@ -0,0 +1,13 @@
+﻿//Fazer um programa para ler um número inteiro e dizer se este número é par ou ímpar.
+
+Console.Write("Digite um número inteiro: ");
+int numero = int.Parse(Console.ReadLine());
+
+string mensagem = NumeroPar(numero) ? $"{numero} é par" : $"{numero} é ímpar";
+
+Console.WriteLine(mensagem);
+
+bool NumeroPar(int n)
+{
+    return n % 2 == 0;
+}
