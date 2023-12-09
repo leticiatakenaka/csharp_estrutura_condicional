d69ac5f9a49c94f6cceeff697fe71862eada4712
Leticia Santos Takenaka <leticiatakenaka12@gmail.com>
2023-12-09 20:44:10 -0300

    Exercício 4: Duração horas do jogo, formato 24 horas

---
+++ Exercicio_04/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Console.WriteLine("Digite a hora inicial e a hora final do jogo (formato 24 horas, separadas por um espaço):");
+string[] entrada = Console.ReadLine().Split(' ');
+
+int horaInicial = int.Parse(entrada[0]);
+int horaFinal = int.Parse(entrada[1]);
+
+int duracao = CalcularDuracao(horaInicial, horaFinal);
+
+Console.WriteLine($"O JOGO DUROU {duracao} HORA(S)");
+
+int CalcularDuracao(int horaInicial, int horaFinal)
+{
+    int duracao;
+
+    if (horaInicial < horaFinal)
+    {
+        duracao = horaFinal - horaInicial;
+    }
+    else
+    {
+        duracao = 24 - horaInicial + horaFinal;
+    }
+
+    return duracao;
+}
